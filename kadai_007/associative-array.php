59f7d59721e8e4df68e07b13f72e6ce3a91361ab
tkhr-m <101968075+tkhr-m@users.noreply.github.com>
2023-09-17 13:20:49 +0900

    kadai_007を追加しました。

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <title>7章課題</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <p>
+      <?php
+      $onion = [
+        'name' => 'onion',
+        'price' => 200,
+        'weight' => 160
+      ];
+
+      print_r($onion);
+      ?>
+    </p>
+  </body>
+</html>
