5e4eaea7e03eec952555007c6e7f175f7a35babc
tkhr-m <101968075+tkhr-m@users.noreply.github.com>
2023-09-17 13:03:33 +0900

    kadai_005を修正しました。

---
+++ kadai_005/score.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>5章課題</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $scores = ['score1' => 80, 'score2' => 60, 'score3' => 55, 'score4' => 40, 'score5' => 100, 'score6' => 25, 'score7' => 80, 'score8' => 95, 'score9' => 30, 'score10' => 60];
+
+      $sum = 0;
+
+      foreach ($scores as $key => $score) {
+        $sum += $scores[$key];
+      }
+
+      $average = $sum / 10;
+
+      echo $average;
+      ?>
+    </p>
+  </body>
+</html>
